1fdd05afdd001f4b2474ac8318d972a4e0e345d4
zoubingwu <zoubingwu@gmail.com>
2019-05-14 11:41:28 +0800

    update

--- renderer.js
+++ renderer.js
@@ -1,3 +1,6 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const lib = require('./build/Release/hello.node');
+
+document.write('hello ' + lib.hello());
